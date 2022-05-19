3fdab2b55fd3e834c5ab442e94cb4b07ca160b73
John Marder <47535784+johnn623@users.noreply.github.com>
2022-05-19 11:23:15 -0400

    Update noisev1.py

--- noisev1.py
+++ noisev1.py
@@ -1,3 +1,8 @@
+# input video file.
+# output will be inserted into a text file named "noise_algorithm_output.txt"
+# score is between 0 - 1, where 1 is high quality, 0 is low quality (more noise
+# will contain a lower score)
+
 import cv2
 import numpy as np
 
@@ -6,9 +11,9 @@ def noise(path):
     ddepth = cv2.CV_16S
     kernel_size = 3
     src = cv2.cvtColor(path, cv2.COLOR_RGB2YCrCb)
-    dst = cv2.Laplacian(src[:, :, 0], ddepth, ksize=kernel_size)
-    abs_dst = cv2.convertScaleAbs(dst)
-    score = 1 - ((np.var(abs_dst) - 1000) / 8000)
+    src = cv2.Laplacian(src[:, :, 0], ddepth, ksize=kernel_size)
+    output = cv2.convertScaleAbs(src)
+    score = 1 - ((np.var(output) - 1000) / 8000)
     if score > 1:
         score = 1
     elif score < 0:
@@ -16,20 +21,16 @@ def noise(path):
     return score
 
 
-img = 'image.png'  # original image
-img = cv2.imread(img)
-cv2.imwrite("f.jpg", img)
-print(f"image.png: {noise(img)}")
+cap = cv2.VideoCapture('input.mp4')  # insert video here
+count = 0
+f = open("noise_algorithm_output.txt", "x")
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret == 0:
+        break
+    # (save image to file system)
+    # cv2.imwrite(f"video frame {count+1}.jpg", frame)
+    f.write(f"Frame: {count+1}, Score: {noise(frame)}\n")
+    count = count + 1
 
-
-img = 'image_noise1.png'
-img = cv2.imread(img)
-print(f"image_noise1.png: {noise(img)}")
-
-img = 'image_noise2.png'
-img = cv2.imread(img)
-print(f"image_noise2.png: {noise(img)}")
-
-img = 'image_noise3.png'
-img = cv2.imread(img)
-print(f"image_noise3.png: {noise(img)}")
+cap.release()
