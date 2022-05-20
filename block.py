10a189f7850d6e83967e2c50a239b2c013873c9c
John Marder <47535784+johnn623@users.noreply.github.com>
2022-05-20 19:47:03 -0400

    Update block metric

--- block.py
+++ block.py
@@ -5,9 +5,9 @@ import numpy as np
 def iteration(img, row, col):
     cropped_image = img[row:row+50, col:col+50]  # crop image
     kernel = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
-    sharpened_block = cv2.filter2D(cropped_image, -1, kernel)
+    sharpened_block = cv2.filter2D(cropped_image, -1, kernel)  # sharpen image
     blocky_canny = cv2.Canny(sharpened_block, 50, 90)
-    return blocky_canny
+    return blocky_canny  # new cropped image
 
 
 def block(img):
@@ -16,17 +16,18 @@ def block(img):
     cropped_image = blockyIm[0:50, 0:50]  # crop image
     kernel = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])  # 8 to 9
     sharpened_block = cv2.filter2D(
-        cropped_image[:, :, 0], -1, kernel)  # sharping
+        cropped_image[:, :, 0], -1, kernel)  # sharping image
     blocky_canny = cv2.Canny(sharpened_block, 50, 90)
-    area = np.array([255, 255, 255, 255, 255])
+    area = np.array([255, 255, 255, 255, 255])  # line of 5 white pixels
     row = 0
     col = 0
-    x = 0
+    count = 0
     r = 0
     c = 0
     while True:
+        # see if there is a line
         if np.array_equal(area, blocky_canny[row:row+5, col]) == 1:
-            x = x + 1
+            count = count + 1
         row = row + 5
         if row == 50:
             col = col + 1
@@ -36,26 +37,25 @@ def block(img):
                 if r == 300:
                     c = c + 50
                     r = 0
-                #print(f"r = {r}, c = {c}")
                 if c != 500:
+                    # create new cropped image
                     blocky_canny = iteration(blockyIm, r, c)
                     row = 0
                     col = 0
                 else:
-                    break
-
-    print(f"number of 'yes': {x}")
+                    break  # the image has been fully cropped
+    if count < 550 and count > 200:
+        count = count - 200
+    elif count > 550:
+        count = count - 150
+    score = 1-(count / 900)
+    if score < 0:
+        score = 0
+    return score
 
 
 originalIm = cv2.imread("originalimage.jpg")
 blockyIm = cv2.imread("compressedimage.jpg")
 
-og = cv2.imread("frame463.jpg")
-block(og)
-og1 = cv2.imread("frame463_block1.jpg")
-block(og1)
-
-og2 = cv2.imread("frame463_block2.jpg")
-block(og2)
-og3 = cv2.imread("frame463_block3.jpg")
-block(og3)
+print(f"originalimage.jpg: {block(originalIm)}")
+print(f"compressedimage.jpg: {block(blockyIm)}")
