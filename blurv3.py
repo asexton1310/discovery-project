302047c3e118339d4a3f9e43fcd010a28ba69a10
John Marder <47535784+johnn623@users.noreply.github.com>
2022-06-02 13:52:40 -0400

    Fixed bug in blurv3.py

--- blurv3.py
+++ blurv3.py
@@ -8,21 +8,25 @@ import numpy as np
 
 
 def sobel_blur(path):
-    im = cv2.resize(path, (800, 600))
-    im = cv2.cvtColor(im, cv2.COLOR_RGB2YCrCb)
-    grad_x = cv2.Sobel(im[:, :, 0], cv2.CV_64F, 0, 1)  # horizontal sobel
-    grad = np.sqrt(grad_x**2)  # magnitude
-    grad_norm_x = (grad * 255 / grad.max()
-                   ).astype(np.uint8)  # normalizing image
-    grad_y = cv2.Sobel(im[:, :, 0], cv2.CV_64F, 1, 0)  # vertical sobel
-    grad = np.sqrt(grad_y**2)  # magnitude
-    grad_norm_y = (grad * 255 / grad.max()
-                   ).astype(np.uint8)  # normalizing image
-    luma = (grad_norm_x + grad_norm_y)/2
-    score = np.var(luma)/380
-    if score > 1:
-        score = 1
-    return score
+    if np.mean(path) == 0:  # if frame is a solid color
+        return 0
+    else:
+        im = cv2.resize(path, (800, 600))
+        im = cv2.cvtColor(im, cv2.COLOR_RGB2YCrCb)
+        grad_x = cv2.Sobel(im[:, :, 0], cv2.CV_64F, 0, 1)  # horizontal sobel
+        grad = np.sqrt(grad_x**2)  # magnitude
+        grad_norm_x = (grad * 255 / grad.max()
+                       ).astype(np.uint8)  # normalizing image
+        grad_y = cv2.Sobel(im[:, :, 0], cv2.CV_64F, 1, 0)  # vertical sobel
+        grad = np.sqrt(grad_y**2)  # magnitude
+        grad_norm_y = (grad * 255 / grad.max()
+                       ).astype(np.uint8)  # normalizing image
+        luma = (grad_norm_x + grad_norm_y)/2
+        score = np.var(luma)/380
+        if score > 1:
+            score = 1
+        return score
+
 
 if __name__ == "__main__":
 
