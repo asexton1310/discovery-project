7fc1ecf9ca4bf98debc8dd0f1b836d3637679686
asexton1310 <89419909+asexton1310@users.noreply.github.com>
2022-06-06 15:45:12 -0400

    Update model_predict.py
    Added support for integrating model_predict with quality_extractor_mpv2.py using folder watchdog.

--- model_predict.py
+++ model_predict.py
@@ -1,40 +1,93 @@
 import pandas as pd
 import tensorflow as tf
 import custom_tf_metrics as custom_metrics
+import logging
+import os.path
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+import time
 
-df = pd.read_csv('live-nrqe.csv')
+class NewMetricEventHandler(PatternMatchingEventHandler):
+    # filesystem watchdog for integrating quality metric extractor with
+    # trained model predictions.
+    # based on examples from python watchdog library available at:
+    # https://github.com/gorakhargosh/watchdog/
 
-#remove video title, since this is not necessary for training
-df.drop('video',axis=1, inplace=True)
-
-# split dataset into training/testing
-train_df = df.sample(frac=0.75, random_state=4)
-test_df = df.drop(train_df.index)
-
-print( train_df.head() )
-#normalize data into range of (0,1)
-max_val = train_df.max(axis= 0)
-min_val = train_df.min(axis= 0)
-data_range = max_val - min_val
-train_df = (train_df - min_val)/(data_range)
-test_df =  (test_df - min_val)/data_range
-
-# separate data into inputs and targets
-X_train = train_df.drop('mos',axis=1)
-X_test = test_df.drop('mos',axis=1)
-y_train = train_df['mos']
-y_test = test_df['mos']
-
-new_model = tf.keras.models.load_model("./savedModels/feedfw-nn-1/", 
+    def __init__(self, model_loc=None, **kwargs):
+        super().__init__(**kwargs)
+        
+        # load model used to make predictions
+        self.model = tf.keras.models.load_model(model_loc,
                                         custom_objects={"PearsonCorrelation":custom_metrics.PearsonCorrelation,
                                                         "SpearmanCorrelation":custom_metrics.SpearmanCorrelation})
+        # Check its architecture
+        print(self.model.summary())
 
-# Check its architecture
-print(new_model.summary())
+    def on_any_event(self, event):
+        #for testing purposes, log every event
+        logging.info(event)
+    
+    def on_created(self, event):
+        # when a new csv file is created, process the previous one
+        # this avoids any potential issues with processing a newly
+        # created csv file before it is finished being written to
+        
+        # split csv number and extension from the rest of the path
+        prefix, num_ext = event.src_path.rsplit("-", 1)
+        # split csv number and extension
+        sample_num, ext = os.path.splitext(num_ext)
+        # get the previous csv file's number
+        target_num = int(sample_num) - 1
 
-# verify loaded model can make predictions
-print("Predictions: ")
-print(new_model.predict(X_test.iloc[0:3, :]))
-print("Truth: ")
-print(y_test.iloc[0:3])
+        if target_num < 0:
+            print(f"No target, fnum too low")
+            return
 
+        # reconstruct path to previous csv file (target file)
+        target_file = f"{prefix}-{target_num}{ext}"
+        print(f"Target File: {target_file}")
+       
+        # read the target csv file
+        df = pd.read_csv(target_file)
+        print("df.head: ", df.head() )
+
+        # make the predictions
+        print("Predictions: ")
+        print(self.model.predict(df.iloc[0:1]))
+        
+        # delete the csv file now that we have made predictions
+        os.remove(target_file)
+
+def getMetrics(path):
+    # Function that repeatedly polls a directory looking for
+    # new csvs (extracted metrics) and uses an observer to
+    # make predictions and delete them when the next csv is 
+    # available.  Modified from quickstart example in
+    # Python watchdog library documentation
+    # https://github.com/gorakhargosh/watchdog
+    #
+    # path - folder to watch for new csvs
+
+    # set up logging for debugging
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    
+    #set up the event handler we want the watchdog observer to use
+    event_handler = NewMetricEventHandler(patterns=['*.csv'],
+                                         ignore_directories=True,
+                                         model_loc="./savedModels/feedfw-nn-1/")
+    # Create, configure, and start the observer                                     
+    observer = Observer()
+    observer.schedule(event_handler, path)
+    observer.start()
+    # while loop with exception so the user is able to interrupt execution
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+if __name__ == "__main__":
+    getMetrics("./quality-metrics/")
