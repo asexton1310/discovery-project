ee6d8c3a048b56014774de9019f1780b85572531
asexton1310 <89419909+asexton1310@users.noreply.github.com>
2022-07-06 23:11:39 -0400

    update model_predict for database connection
    Updated model_predict for uploading data to our AWS database.
    
    Deleted duplicate CSVs outside of the nn-csvs folder to stay organized.

--- model_predict.py
+++ model_predict.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from numpy import typename
 import tensorflow as tf
 import custom_tf_metrics as custom_metrics
 import logging
@@ -6,14 +7,17 @@ import os.path
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 import time
+import mysql.connector
+import sqlconnecterFINAL
 
 class NewMetricEventHandler(PatternMatchingEventHandler):
-    # filesystem watchdog for integrating quality metric extractor with
-    # trained model predictions.
-    # based on examples from python watchdog library available at:
-    # https://github.com/gorakhargosh/watchdog/
+    """ filesystem watchdog for integrating quality metric extractor with
+        trained model predictions.
+        based on examples from python watchdog library available at:
+        https://github.com/gorakhargosh/watchdog/
+    """
 
-    def __init__(self, model_loc=None, **kwargs):
+    def __init__(self, model_loc=None, mysql_connection=None, **kwargs):
         super().__init__(**kwargs)
         
         # load model used to make predictions
@@ -22,6 +26,21 @@ class NewMetricEventHandler(PatternMatchingEventHandler):
                                                         "SpearmanCorrelation":custom_metrics.SpearmanCorrelation})
         # Check its architecture
         print(self.model.summary())
+
+        """ set the database connection for future updates 
+            TODO figure out how to set synchronize the stream_id based on some inherent fact of the stream
+                 so that multiple local estimation systems can synchronize to have the same stream_id when they
+                 are recording the quality of a single stream at different locations
+            TODO location_id should be something unique to the specific location the stream is getting sampled at. 
+                 Something like IP (if privacy is not a concern) or some unique hardware identifier could
+                 be used to signify the specific locatino quality info corresponds to.
+        """
+        stream_id = time.time() # for demos in this project, this will just be a timestamp of when the stream started. 
+                                # this only works with 1 local estimation system as multiple will not be synchronized
+        location_id = 1 # for demos in this project, this is fixed at 1
+
+        self.db_stream = sqlconnecterFINAL.Stream(mysql_connection, stream_id, location_id)
+        self.db_stream.initializeDB()
 
     def on_any_event(self, event):
         #for testing purposes, log every event
@@ -52,11 +71,29 @@ class NewMetricEventHandler(PatternMatchingEventHandler):
         print("df.head: ", df.head() )
 
         # make the predictions
+        quality_estimate = self.model.predict(df.iloc[0:1]) # quality_estimate in form [[numpyFloat]]
         print("Predictions: ")
-        print(self.model.predict(df.iloc[0:1]))
-        
+        print(quality_estimate)
+
+        # update the database
+        metric_labels = list(df.columns)
+        for metric in metric_labels:
+            self.db_stream.datapoints[metric] = df.loc[0,metric]
+
+        self.db_stream.datapoints["quality_estimate"] = float(quality_estimate[0][0])    # minor reformat of quality_estimate to float for DB compatibility
+        #print(self.db_stream.datapoints)
+
+        self.db_stream.injectData()
+
         # delete the csv file now that we have made predictions
         os.remove(target_file)
+
+    def end_stream(self):
+        """ updates the stream's status in the database to indicate the stream is over
+            then closes the connection to the database
+        """
+        self.db_stream.offline()
+        self.db_stream.closeDBstream()
 
 def getMetrics(path, model_path):
     # Function that repeatedly polls a directory looking for
@@ -73,10 +110,21 @@ def getMetrics(path, model_path):
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
     
+    # set up database connection
+    try:
+        connectionDB = mysql.connector.connect(user='disctest', password='test', database='discoveryproject',
+                              host='localhost',
+                       )
+        print("Connection String Opened successfully")
+    except Exception as e:
+        print("**ERROR HAS OCCURED**: This was your error: ", e)
+        print("Make sure the username and password and hostname are correct. If the database is offline contact your server admin (Juan)")
+
     #set up the event handler we want the watchdog observer to use
     event_handler = NewMetricEventHandler(patterns=['*.csv'],
                                          ignore_directories=True,
-                                         model_loc=model_path)
+                                         model_loc=model_path,
+                                         mysql_connection=connectionDB)
     # Create, configure, and start the observer                                     
     observer = Observer()
     observer.schedule(event_handler, path)
@@ -86,8 +134,11 @@ def getMetrics(path, model_path):
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
+        event_handler.end_stream()
         observer.stop()
+        connectionDB.close()
     observer.join()
 
 if __name__ == "__main__":
-    getMetrics("./quality-metrics/", model_path="./savedModels/feedfw-nn-2022-06-09_18-30-34/")
+    #getMetrics("./quality-metrics/", model_path="./savedModels/feedfw-nn-2022-06-09_18-30-34/")
+    getMetrics("./quality-metrics/", model_path="./savedModels/live-ls-nrqe-2022-07-02_21-40-51/")
